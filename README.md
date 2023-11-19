b47f89e77d5150558e2c02c82c7908e85e5107dc
beratagun <berat.agun@gmail.com>
2023-11-19 16:15:22 +0300

    Upload

---
+++ README.md
@@ -0,0 +1,51 @@
+# Tourism Agency System
+
+## Project Description:
+- This is a project for our Patika+ Bootcamp, in which we have to develop a system for a tourism agency.
+- The system must be able to manage the agency's customers, employees, and reservations.
+ 
+
+## Project Structure:
+- There are two main user types, admin and agency employee.
+- The admin is responsible for managing the agency's employees; she/he can create new admin and employee profiles.
+
+## Login Panel:
+- The user can login to the system with her/his username and password.
+
+
+
+## Admin Panel:
+- The admin can create new admin and employee profiles.
+
+
+
+## Employee Panel:
+
+### Management:
+
+- The employee can create:
+    - hotels,
+
+    - rooms for each hotel,
+
+    - and prices for each room with a different accommodation type.
+
+
+### Sales:
+
+- The employee can create a new reservation for a customer by:
+    - searching for an available room using the search panel,
+
+    - selecting the accommodation type and calculating the price,
+
+    - and providing the customer's information.
+
+
+
+### Log:
+- Here, the employee can see:
+            
+  - all the reservations created
+  - and all current and previous guests.
+
+              
---
+++ src/com/TourismAgencySystem/View/LoginGUI.java
@@ -0,0 +1,139 @@
+package com.TourismAgencySystem.View;
+
+import com.TourismAgencySystem.Helper.Config;
+import com.TourismAgencySystem.Helper.Helper;
+import com.TourismAgencySystem.Model.Admin;
+import com.TourismAgencySystem.Model.Employee;
+import com.TourismAgencySystem.Model.User;
+import com.TourismAgencySystem.Model.UserOp;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class LoginGUI extends JFrame {
+    private JPanel wrapper;
+    private JPanel wtop;
+    private JPanel wbottom;
+    private JTextField fieldUsername;
+    private JPasswordField fieldPassword;
+    private JButton buttonLogin;
+    private JCheckBox checkBoxShowPass;
+
+    public LoginGUI() {
+        add(wrapper);
+        setSize(400, 400);
+        setLocation(Helper.screenCenterLocation("x", getSize()), Helper.screenCenterLocation("y", getSize()));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle(Config.PROJECT_TITLE);
+        setResizable(false);
+        setVisible(true);
+        //Değerlendirme Formu 8
+        // Login işleminde kullanıcının kaydı olup olmadığı kontrol ediliyor mu, kaydı yoksa ya da hatalı giriş yapıldıysa kullanıcıya hatalı giriş mesajı veriliyor mu?
+        buttonLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Helper.isFieldEmpty(fieldUsername) || Helper.isFieldEmpty(fieldPassword)) {
+                    Helper.showMessage("fill");
+                } else {
+                    User u = UserOp.getFetch(fieldUsername.getText(), fieldPassword.getText());
+                    if (u == null) {
+                        //Değerlendirme Formu 20
+                        //Kullanıcıya hatalı işlemler için uygun hata mesajları veriliyor mu?
+                        Helper.showMessage("Please check your username and password !");
+                    } else {
+                        switch (u.getType()) {
+                            case "admin":
+                                AdminGUI adminGUI = new AdminGUI((Admin) u);
+                                break;
+                            case "employee":
+                                EmployeeGUI employeeGUI = new EmployeeGUI((Employee) u);
+                                break;
+
+                        }
+                        dispose();
+                    }
+                }
+            }
+        });
+        //Değerlendirme Formu 8
+        // Login işleminde kullanıcının kaydı olup olmadığı kontrol ediliyor mu, kaydı yoksa ya da hatalı giriş yapıldıysa kullanıcıya hatalı giriş mesajı veriliyor mu?
+        fieldUsername.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if (Helper.isFieldEmpty(fieldUsername) || Helper.isFieldEmpty(fieldPassword)) {
+                        Helper.showMessage("fill");
+                    } else {
+                        User u = UserOp.getFetch(fieldUsername.getText(), fieldPassword.getText());
+                        if (u == null) {
+                            Helper.showMessage("Please check your username and password !");
+                        } else {
+                            switch (u.getType()) {
+                                case "admin":
+                                    AdminGUI adminGUI = new AdminGUI((Admin) u);
+                                    break;
+                                case "employee":
+                                    EmployeeGUI employeeGUI = new EmployeeGUI((Employee) u);
+                                    break;
+
+                            }
+                            dispose();
+                        }
+                    }
+                }
+            }
+        });
+        //Değerlendirme Formu 8
+        // Login işleminde kullanıcının kaydı olup olmadığı kontrol ediliyor mu, kaydı yoksa ya da hatalı giriş yapıldıysa kullanıcıya hatalı giriş mesajı veriliyor mu?
+        fieldPassword.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+
+
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if (Helper.isFieldEmpty(fieldUsername) || Helper.isFieldEmpty(fieldPassword)) {
+                        Helper.showMessage("fill");
+                    } else {
+                        User u = UserOp.getFetch(fieldUsername.getText(), fieldPassword.getText());
+                        if (u == null) {
+                            Helper.showMessage("Please check your username and password !");
+                        } else {
+                            switch (u.getType()) {
+                                case "admin":
+                                    AdminGUI adminGUI = new AdminGUI((Admin) u);
+                                    break;
+                                case "employee":
+                                    EmployeeGUI employeeGUI = new EmployeeGUI((Employee) u);
+                                    break;
+
+                            }
+                            dispose();
+                        }
+                    }
+                }
+
+            }
+        });
+
+        checkBoxShowPass.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (checkBoxShowPass.isSelected()) {
+                    fieldPassword.setEchoChar((char) 0);
+                } else {
+                    fieldPassword.setEchoChar('*');
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Helper.setLayout();
+        LoginGUI loginGUI = new LoginGUI();
+    }
+}
